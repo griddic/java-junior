f6b2f0e4409df80c2a0fed3b04f244bbb3ad4142
griddic <griddic@mail.ru>
2016-08-23 16:52:52 +0300

    specify nik name for Grigorii Koksharov

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -32,7 +32,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutEquals("primitive: 1" + System.lineSeparator() + "primitive: 0" + System.lineSeparator() + "primitive: -1" + System.lineSeparator() );
         //endregion
     }
 
@@ -52,8 +52,8 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
-    /*
-    TODO: implement Logger solution to match specification as tests
+
+    //TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogChar() throws IOException {
@@ -69,6 +69,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
+    /*
     @Test
     public void shouldLogString() throws IOException {
         //region when
