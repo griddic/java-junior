fa461f50aff07a9c872f663b51095f90ce593a29
griddic <griddic@mail.ru>
2016-08-24 10:24:54 +0300

    add formatter + simpliest handler

---
+++ src/main/java/com/acme/edu/Handler.java
@@ -0,0 +1,16 @@
+package com.acme.edu;
+/**
+ * Created by Java_5 on 23.08.2016.
+ */
+/*
+public class Handler {
+    public static void write (String message) {
+
+    }
+}
+*/
+class STD{
+    public static void write (String message) {
+        System.out.print(message);
+    }
+}
--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,30 +1,57 @@
 package com.acme.edu;
 
+import java.util.Objects;
+
 public class Logger {
-    public static void logAnyPrimitiveType(Object message) {
-        System.out.println("primitive: " + message);
+    static STD handler = new STD();
+
+    public static void logRawString(String message) {
+        handler.write(message);
+    }
+
+
+    public static void logLnRawString(String message) {
+        logRawString(message + System.lineSeparator());
     }
 
     public static void log(int message) {
-        logAnyPrimitiveType(message);
+        logLnRawString(Formatter.anyPrimitiveType(message));
     }
 
     public static void log(byte message) {
-        logAnyPrimitiveType(message);
+        logLnRawString(Formatter.anyPrimitiveType(message));
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        logLnRawString(Formatter.char_(message));
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        logLnRawString(Formatter.string_(message));
     }
 
     public static void log(boolean message) {
-        logAnyPrimitiveType(message);
+        logLnRawString(Formatter.anyPrimitiveType(message));
     }
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        logLnRawString(Formatter.object_(message));
+    }
+}
+
+class Formatter {
+    static String format(String template, Object msg) {
+        return String.format(template, msg);
+    }
+    public static String char_ (char ch) {
+        return format("char: %s", ch);
+    }
+    public static String string_(String str) {
+        return format("string: %s", str);
+    }
+    public static String object_(Object obj) {
+        return format("reference: %s", obj);
+    }
+    public static String anyPrimitiveType(Object obj) {
+        return format("primitive: %s", obj);
     }
 }
