2b48e5d9960ddec58697b63adbfcedc00a4b4cb7
griddic <griddic@mail.ru>
2016-08-24 12:04:05 +0300

    refactoring: Formatter.format -> String.format, formatter as variable

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Logger {
     static STD handler = new STD();
+    static Formatter formatter = new Formatter();
 
     public static void logRawString(String message) {
         handler.write(message);
@@ -15,43 +16,40 @@ public class Logger {
     }
 
     public static void log(int message) {
-        logLnRawString(Formatter.anyPrimitiveType(message));
+        logLnRawString(formatter.anyPrimitiveType(message));
     }
 
     public static void log(byte message) {
-        logLnRawString(Formatter.anyPrimitiveType(message));
+        logLnRawString(formatter.anyPrimitiveType(message));
     }
 
     public static void log(char message) {
-        logLnRawString(Formatter.char_(message));
+        logLnRawString(formatter.char_(message));
     }
 
     public static void log(String message) {
-        logLnRawString(Formatter.string_(message));
+        logLnRawString(formatter.string_(message));
     }
 
     public static void log(boolean message) {
-        logLnRawString(Formatter.anyPrimitiveType(message));
+        logLnRawString(formatter.anyPrimitiveType(message));
     }
     public static void log(Object message) {
-        logLnRawString(Formatter.object_(message));
+        logLnRawString(formatter.object_(message));
     }
 }
 
 class Formatter {
-    static String format(String template, Object msg) {
-        return String.format(template, msg);
-    }
     public static String char_ (char ch) {
-        return format("char: %s", ch);
+        return String.format("char: %s", ch);
     }
     public static String string_(String str) {
-        return format("string: %s", str);
+        return String.format("string: %s", str);
     }
     public static String object_(Object obj) {
-        return format("reference: %s", obj);
+        return String.format("reference: %s", obj);
     }
     public static String anyPrimitiveType(Object obj) {
-        return format("primitive: %s", obj);
+        return String.format("primitive: %s", obj);
     }
 }
