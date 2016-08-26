1ce65f54fee78ad75b61f3ccabd05311e376227b
PavelEvdokimov <evdokimovpascha@gmail.com>
2016-08-26 18:06:53 +0300

    More test coverage

--- src/test/java/com/acme/edu/UnitTests/LoggerTest.java
+++ src/test/java/com/acme/edu/UnitTests/LoggerTest.java
@@ -28,6 +28,7 @@ public class LoggerTest {
         //Then
         verify(outStream).write("primitive: 5" + System.lineSeparator());
     }
+
     @Test
     public void shouldAccumulateNumbersForByteSequences() {
         //Given
@@ -41,4 +42,59 @@ public class LoggerTest {
         //Then
         verify(outStream).write("primitive: 5" + System.lineSeparator());
     }
+
+    @Test
+    public void shouldAccumulateStringsForStringSequences() {
+        //Given
+        Formatter formatter = mock(Formatter.class);
+        OutStream outStream = mock(OutStream.class);
+        Logger logger = new Logger (formatter, outStream);
+        when(formatter.decorateStringsSequence("str 1", 2)).thenReturn("str 1 (x2)");
+        //When
+        logger.log("str 1");
+        logger.log("str 1");
+        logger.strSequenceEnd();
+        //Then
+        verify(outStream).write("str 1 (x2)" + System.lineSeparator());
+    }
+
+    @Test
+    public void shouldLogIntArrays () {
+        //Given
+        Formatter formatter = mock(Formatter.class);
+        OutStream outStream = mock(OutStream.class);
+        Logger logger = new Logger (formatter, outStream);
+        when(formatter.decorArray(new int[] {1,2,3,4,5}))
+                .thenReturn("primitives array: {1, 2, 3, 4, 5}");
+        //When
+        logger.log(new int[] {1,2,3,4,5});
+        //Then
+        verify(outStream).write("primitives array: {1, 2, 3, 4, 5}"
+                + System.lineSeparator());
+    }
+
+    /*@Test
+    public void shouldLogIntMatrix () {
+        //Given
+        Formatter formatter = mock(Formatter.class);
+        OutStream outStream = mock(OutStream.class);
+        Logger logger = new Logger (formatter, outStream);
+        when(formatter.decorMatrix(new int[][] {{1,2,3},{3,2,1}}))
+                .thenReturn("primitives matrix: {" + System.lineSeparator() +
+                        "{1, 2, 3}" + System.lineSeparator() +
+                        "{3, 2, 1}" + System.lineSeparator() +
+                        "}" + System.lineSeparator());
+        //When
+        logger.log(new int[][] {{1,2,3}, {3,2,1}});
+        System.out.println(formatter.decorMatrix(new int[][] {{1,2,3},{3,2,1}}));
+        System.out.println("primitives matrix: {" + System.lineSeparator() +
+                "{1, 2, 3}" + System.lineSeparator() +
+                "{3, 2, 1}" + System.lineSeparator() +
+                "}" + System.lineSeparator());
+        //Then
+        verify(outStream).write("primitives matrix: {" + System.lineSeparator() +
+                "{1, 2, 3}" + System.lineSeparator() +
+                "{3, 2, 1}" + System.lineSeparator() +
+                "}" + System.lineSeparator());
+    }*/
 }
