137cb803eac37aa1d8821ba1a23bd9c03f09093e
PavelEvdokimov <evdokimovpascha@gmail.com>
2016-08-29 15:56:06 +0300

    Added some javadocs

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -13,6 +13,11 @@ public class Logger {
     String lastStringInStringsSequence = null;
     int quantityOfLastStringRepeated = 0;
 
+    /**
+     * This is constructor method for Logger.
+     * @param formatter allows to configure decoration for data being logged.
+     * @param outStreams allows to configure one ore many output streams.
+     */
     public Logger(Formatter formatter, OutStream... outStreams) {
         this.outStreams = new OutputsContainer(outStreams);
         this.formatter = formatter;
@@ -37,18 +42,21 @@ public class Logger {
     }
 
     /** Method log (int) accumulates values for integer sequences.
-     * In the end of any integer sequence intSequenceEnd() method must be called.
+     * Input data must be "int" type.
+     * Method accumulates sequenced integers.
+     * To show accumulated data, in the end of any integer sequence intSequenceEnd() method must be called.
      */
     public void log(int number) {
         summatorForIntSequence += number;
         int additionToOverflow = (int) (summatorForIntSequence /Integer.MAX_VALUE);
         ammountOfMaxIntegerOverflow += additionToOverflow;
         summatorForIntSequence = summatorForIntSequence - (Integer.MAX_VALUE * additionToOverflow);
-        //logLnRawString(formatter.decorateAnyPrimitiveType(message));
     }
 
     /** Method log (byte) accumulates values for byte sequences.
-     * In the end of any byte sequence byteSequenceEnd() method must be called.
+     * Input data must be "byte" type.
+     * Method accumulates sequenced integers.
+     * To show accumulated data, in the end of any byte sequence byteSequenceEnd() method must be called.
      */
     public void log(byte number) {
         int additionToOverflow;
@@ -58,16 +66,16 @@ public class Logger {
         summatorForByteSequence = summatorForByteSequence - (Byte.MAX_VALUE * additionToOverflow);
     }
 
-    /** This method logs char data.
-     *
+    /** This method logs char data with configured decoration.
+     * Input data must be "char" type.
      */
     public void log(char message) {
         logLnRawString(formatter.decorateChar(message));
     }
 
     /** Method log (string) accumulates number of repeating string for string sequences.
-     * In the end of any string sequence strSequenceEnd() method must be called.
-     *
+     * Input data must be "String" type.
+     * To show accumulated data, in the end of any string sequence strSequenceEnd() method must be called.
      */
     public void log(String message) {
         if (message.equals(lastStringInStringsSequence)) {
@@ -82,20 +90,35 @@ public class Logger {
     }
 
 
+    /**
+     * Method strSequenceEnd outputs accumulated data for String sequences.
+     * This method will log sequenced repeating strings as one line with number of repeats with configured decoration.
+     */
     public void strSequenceEnd() {
         logLnRawString(formatter.decorateStringsSequence(lastStringInStringsSequence, quantityOfLastStringRepeated));
         lastStringInStringsSequence = null;
         quantityOfLastStringRepeated = 0;
     }
 
-        public void log(boolean message) {
+    /** This method logs boolean data with configured decoration.
+     * Input data must be "boolean" type.
+     */
+    public void log(boolean message) {
         logLnRawString(formatter.decorateAnyPrimitiveType(message));
     }
 
+    /** This method logs object data with configured decoration.
+     * Input data must be "object" type.
+     */
     public void log(Object message) {
         logLnRawString(formatter.decorateObject(message));
     }
 
+    /**
+     * Method intSequenceEnd outputs accumulated data for int sequences with configured decoration.
+     * This method will log sequenced integers as their sum.
+     * If sum is greater, than MAX_Integer, method will output tail after dividing sum by MAX_Integer and than a number of MAX_Integers that makes the correct sum.
+     */
     public void intSequenceEnd() {
         logLnRawString(formatter.decorateAnyPrimitiveType(summatorForIntSequence));
         for (int i = 0; i < ammountOfMaxIntegerOverflow; i++) {
@@ -105,6 +128,11 @@ public class Logger {
         ammountOfMaxIntegerOverflow = 0;
     }
 
+    /**
+     * Method byteSequenceEnd outputs accumulated data for byte sequences with configured decoration.
+     * This method will log sequenced byte numbers as their sum.
+     * If sum is greater, than MAX_Byte, method will output tail after dividing sum by MAX_Byte and than a number of MAX_Bytes that makes the correct sum.
+     */
     public void byteSequenceEnd() {
         logLnRawString(formatter.decorateAnyPrimitiveType(summatorForByteSequence));
         for (int i = 0; i < ammountOfMaxByteOverflow; i++) {
@@ -114,10 +142,16 @@ public class Logger {
         ammountOfMaxByteOverflow = 0;
     }
 
+    /** This method logs integer arrays with configured decoration.
+     * Input data must be "int []" type.
+     */
     public void log(int[] array) {
         logLnRawString(formatter.decorArray(array));
     }
 
+    /** This method logs integer matrixes with configured decoration.
+     * Input data must be "int [][]" type.
+     */
     public void log(int[][] matrix) {
         logLnRawString(formatter.decorMatrix(matrix));
     }
